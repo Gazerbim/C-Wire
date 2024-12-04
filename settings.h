7010a9a4ae77b830e486e5557011cd6ab430f751
QMHussain <hussain.qureshi@etu.cyu.fr>
2024-12-04 21:34:36 +0100

    Update settings.h and data_tree.c

--- settings.h
+++ settings.h
@@ -12,22 +12,27 @@ typedef struct Avl{
     struct Avl *leftSon;
     struct Avl *rightSon;
     int balance;
-    int capacity;
+    long capacity;
+    int id;
+    long load;
 }Avl, *pAvl;
 
 
 int min(int a, int b);
 int max3(int a, int b, int c);
 pAvl createNode();
-pAvl createAVL(int capacity);
-void insert(pAvl tree, int capacity);
+pAvl createAVL(long capacity, int id);
 int getBalance(pAvl node);
 pAvl rotateRight(pAvl node);
 pAvl rotateLeft(pAvl node);
 pAvl doubleRotateLeft(pAvl node);
 pAvl doubleRotateRight(pAvl node);
 pAvl balanceAVL(pAvl node);
-pAvl insertAVL(pAvl node, int capacity, int *h);
+pAvl insertAVL(pAvl node, long capacity, int *h, int id);
+int research(pAvl node, int id, pAvl *searched);
 void printAVL(pAvl node);
+void updateStation(pAvl tree, int id, int load);
+pAvl buildAvl(pAvl tree, int isLv, int isHva, int isHvb, char *chvb, char *chva,
+                char *clv, char *ccomp, char *cindiv, char *ccapa, char *cload);
 
 #endif //SETTINGS_H

2e0c2c4764616ae9e662296121f4a93dacec343b
Gazerbim <100291331+Gazerbim@users.noreply.github.com>
2024-12-11 16:37:07 +0100

    Update process.c

--- process.c
+++ process.c
@@ -1,62 +1,50 @@
 #include "settings.h"
 
-void updateStation(pAvl tree, int id, long load){
+pAvl updateStation(pAvl tree, int id, long load) {
     pAvl station;
-    int result;
-    result = research(tree, id, &station);
-    
-    if(!result){
-        printf("Station not found\n");
-        exit(3);
-    }
-    else{
+    int h = 0;
+    int result = research(tree, id, &station);
+    if (!result) {
+        tree = insertAVL(tree, 0, &h, id);
+    } else {
         station->load += load;
     }
-    
-}
 
-
-pAvl buildAvl(pAvl tree, int isLv, int isHva, int isHvb, char *chvb, char *chva, char *clv, char *ccomp, char *cindiv, char *ccapa, char *cload){
-    int h;
-    if(isLv){
-        if (strcmp("-", ccomp) || strcmp("-", cindiv)){ // this is a consumer
-            updateStation(tree, atoi(clv), atol(cload));
-        }
-    }else if(isHva){
-        if (strcmp("-", ccomp) || strcmp("-", cindiv)){ // this is a consumer
-            updateStation(tree, atoi(chva), atol(cload));
-        }
-        else if(strcmp("-", clv)){ // this is a lv station
-            updateStation(tree, atoi(chva), atol(ccapa));
-        }
-    }else if(isHvb){
-        if (strcmp("-", ccomp) || strcmp("-", cindiv)){ // this is a consumer
-            updateStation(tree, atoi(chvb), atol(cload));
-        }
-        else if(strcmp("-", clv) || strcmp("-", chva)){ // this is a lv or hva
-            updateStation(tree, atoi(chvb), atol(ccapa));
-        }
-    }
     return tree;
 }
 
-pAvl buildStations(pAvl tree, int isLv, int isHva, int isHvb, char *chvb, char *chva, char *clv, char *ccomp, char *cindiv, char *ccapa, char *cload){
-    int h;
-    if(isLv){
-        if (!strcmp("-", ccomp) && !strcmp("-", cindiv)){ // this is a lv station
-            tree = insertAVL(tree, atol(ccapa), &h, atol(clv));
+
+
+pAvl buildAvl(pAvl tree, int isLv, int isHva, int isHvb, char *chvb, char *chva, char *clv, char *ccomp, char *cindiv, char *ccapa, char *cload) {
+    int h = 0;
+    if (isLv) {
+        if (strcmp("-", chva)) { // This is an lv station
+            tree = insertAVL(tree, atol(ccapa), &h, atoi(clv));
+        }else {
+            tree = updateStation(tree, atoi(clv), atol(cload));
         }
-    }else if(isHva){
-        if(strcmp("-", chva)){ // this is a hva station
-            tree = insertAVL(tree, atol(ccapa), &h, atol(chva));
+    } else if (isHva) {
+        if (strcmp("-", ccomp) != 0 || strcmp("-", cindiv) != 0) { // This is a consumer
+            tree = updateStation(tree, atoi(chva), atol(cload));
+        }else if (strcmp("-", clv) != 0) { // This is an lv station
+            tree = updateStation(tree, atoi(chva), atol(ccapa));
+        }else {
+            tree = insertAVL(tree, atol(ccapa), &h, atoi(chva));
         }
-    }else if(isHvb){
-        if(strcmp("-", chvb)){ // this is a hvb station
-            tree = insertAVL(tree, atol(ccapa), &h, atol(chvb));
+    } else if (isHvb) {
+        if (strcmp("-", ccomp) != 0 || strcmp("-", cindiv) != 0) { // This is a consumer
+            tree = updateStation(tree, atoi(chvb), atol(cload));
+        }else if (!strcmp("-", chva)) { // This is an hva station
+            tree = updateStation(tree, atoi(chvb), atol(ccapa));
+        }else {
+            tree = insertAVL(tree, atol(ccapa), &h, atoi(chvb));
         }
     }
+
     return tree;
 }
+
+
 
 
 pAvl readDataAndBuildAVL(pAvl tree, FILE * file,int isLv, int isHva, int isHvb){
@@ -65,25 +53,6 @@ pAvl readDataAndBuildAVL(pAvl tree, FILE * file,int isLv, int isHva, int isHvb){
     char line[256];
     rewind(file);
     int i=0;
-    // read each line the first time to build the stations
-    while (fgets(line, sizeof(line), file)) {
-        // delete line jump
-        line[strcspn(line, "\n")] = '\0';
-
-        // cut each string with ';'
-        cpp = strtok(line, ";");
-        chvb = strtok(NULL, ";");
-        chva = strtok(NULL, ";");
-        clv = strtok(NULL, ";");
-        ccomp = strtok(NULL, ";");
-        cindiv = strtok(NULL, ";");
-        ccapa = strtok(NULL, ";");
-        cload = strtok(NULL, ";");
-        //printf("%s %s %s %s %s %s %s %s\n", cpp, chvb, chva, clv, ccomp, cindiv, ccapa, cload);
-        
-        tree = buildStations(tree, isLv, isHva, isHvb, chvb, chva, clv, ccomp, cindiv, ccapa, cload);
-    }
-    rewind(file);
     // read each line the second time to sum the consuptions
     while (fgets(line, sizeof(line), file)) {
         i++;

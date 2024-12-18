41775092e92b4b6d7b44a42c0d7ed46d8e9ca72a
Gazerbim <100291331+Gazerbim@users.noreply.github.com>
2024-12-18 13:52:27 +0100

    Update data_transfer.c

--- data_transfer.c
+++ data_transfer.c
@@ -10,21 +10,31 @@ void fprintAVL(pAvl node, FILE *file) {
 }
 
 //will write the header and launch the fprintAVL function
-void transferToFile(pAvl tree, int isLv, int isHva, int isHvb) {
+void transferToFile(pAvl tree, int isLv, int isHva, int isHvb, char* station, char* consumer, char* powerplant) {
     FILE *file;
-    file = fopen("data.csv", "w+");
+    char fileTitle[200]="";
+    strcat(fileTitle, station);
+    strcat(fileTitle, "_");
+    strcat(fileTitle, consumer);
+    if(strcmp("all", powerplant)!=0){ // if we have a specified power plant
+        strcat(fileTitle, "_");
+        strcat(fileTitle, powerplant);
+    }
+    strcat(fileTitle, ".csv");
+    printf("%s\n", fileTitle);
+    file = fopen(fileTitle, "w+");
     if (file == NULL) {
         printf("Error opening file\n");
         exit(4);
     }
     if (isLv) {
-        fprintf(file, "LV Station:Capacity:Consumption\n");
+        fprintf(file, "LV Station:Capacity:Consumption (%s)\n", consumer);
     }
     if (isHva) {
-        fprintf(file, "HVA Station:Capacity:Consumption\n");
+        fprintf(file, "HVA Station:Capacity:Consumption (%s)\n", consumer);
     }
     if (isHvb) {
-        fprintf(file, "HVB Station:Capacity:Consumption\n");
+        fprintf(file, "HVB Station:Capacity:Consumption (%s)\n", consumer);
     }
     fprintAVL(tree, file);
     fclose(file);

c21aa9dc9d3eebe090b186d486373b7747af045c
Gazerbim <100291331+Gazerbim@users.noreply.github.com>
2024-12-18 14:34:07 +0100

    Create main.c

---
+++ CodeC/main.c
@@ -0,0 +1,19 @@
+#include "settings.h"
+
+int main(int argc, char *argv[]){
+    int isLv = 0 , isHva = 0, isHvb = 0;
+    pAvl tree = NULL;
+
+
+    if (strcmp("lv", argv[1])==0){
+        isLv = 1;
+    }else if (strcmp("hva", argv[1])==0){
+        isHva = 1;
+    }else if (strcmp("hvb", argv[1])==0){
+        isHvb = 1;
+    }
+    tree = handleTreeProcess(tree, isLv, isHva, isHvb);
+    transferToFile(tree, isLv, isHva, isHvb, argv[1], argv[2], argv[3]);
+    freeTree(tree);
+    return 0;
+}

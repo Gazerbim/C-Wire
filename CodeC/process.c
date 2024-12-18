6a297a07fe0c97ee353df3e3c5b5e5239cb598bd
Gazerbim <100291331+Gazerbim@users.noreply.github.com>
2024-12-18 15:17:34 +0100

    Update process.c

--- CodeC/process.c
+++ CodeC/process.c
@@ -1,95 +1,40 @@
 #include "settings.h"
 
-/*This function will search for a station and update it's load. 
-    If the station doesn't exist, a new station will be created*/
-pAvl updateStation(pAvl tree, int id, long load) {
-    pAvl station;
-    int h = 0;
-    int result = research(tree, id, &station);
-    if (!result) {
-        tree = insertAVL(tree, 0, &h, id);
-    } else {
-        station->load += load;
+//will write the data in the tree in the file in the id order
+void fprintAVL(pAvl node, FILE *file) {
+    if (node != NULL){
+        fprintAVL(node->leftSon, file);
+        fprintf(file,"%d:%ld:%ld\n", node->id, node->capacity, node->load);
+        fprintAVL(node->rightSon, file);
     }
-
-    return tree;
 }
 
-
-/*This function will recognize in which case we are based in the line of the file and on the executed command 
-    and will perform action based on that*/
-pAvl buildAvl(pAvl tree, int isLv, int isHva, int isHvb, char *chvb, char *chva, char *clv, char *ccomp, char *cindiv, char *ccapa, char *cload) {
-    int h = 0;
-    if (isLv) {
-        if (strcmp("-", chva)!=0) { // This is an lv station
-            tree = insertAVL(tree, atol(ccapa), &h, atoi(clv));
-        }else {
-            tree = updateStation(tree, atoi(clv), atol(cload));
-        }
-    } else if (isHva) {
-        if (strcmp("-", ccomp) != 0 || strcmp("-", cindiv) != 0) { // This is a consumer
-            tree = updateStation(tree, atoi(chva), atol(cload));
-        }else if (strcmp("-", clv) != 0) { // This is an lv station
-            tree = updateStation(tree, atoi(chva), atol(ccapa));
-        }else {
-            tree = insertAVL(tree, atol(ccapa), &h, atoi(chva));
-        }
-    } else if (isHvb) {
-        if (strcmp("-", ccomp) != 0 || strcmp("-", cindiv) != 0) { // This is a consumer
-            tree = updateStation(tree, atoi(chvb), atol(cload));
-        }else if (!strcmp("-", chva)) { // This is an hva station
-            tree = updateStation(tree, atoi(chvb), atol(ccapa));
-        }else {
-            tree = insertAVL(tree, atol(ccapa), &h, atoi(chvb));
-        }
-    }
-
-    return tree;
-}
-
-
-
-/*Will decode informations in the file based on the command executed, and will fill the tree*/
-pAvl readDataAndBuildAVL(pAvl tree, FILE * file,int isLv, int isHva, int isHvb){
-    char *cpp, *chvb, *chva, *clv, *ccomp, *cindiv, *ccapa, *cload;
-    //int pp, hvb, hva, lv, comp, indiv, capa, load; 
-    char line[256];
-    rewind(file);
-    int i=0;
-    // read each line the second time to sum the consuptions
-    
-    while (fgets(line, sizeof(line), file)) {
-        i++;
-        // delete line jump
-        line[strcspn(line, "\n")] = '\0';
-
-        // cut each string with separated with ';'
-        cpp = strtok(line, ";");
-        chvb = strtok(NULL, ";");
-        chva = strtok(NULL, ";");
-        clv = strtok(NULL, ";");
-        ccomp = strtok(NULL, ";");
-        cindiv = strtok(NULL, ";");
-        ccapa = strtok(NULL, ";");
-        cload = strtok(NULL, ";");
-        
-        tree = buildAvl(tree, isLv, isHva, isHvb, chvb, chva, clv, ccomp, cindiv, ccapa, cload);
-    }
-    return tree;
-}
-
-/*Open the file and launch the process*/
-pAvl handleTreeProcess(pAvl tree, int isLv, int isHva, int isHvb){
+//will write the header and launch the fprintAVL function
+void transferToFile(pAvl tree, int isLv, int isHva, int isHvb, char* station, char* consumer, char* powerplant) {
     FILE *file;
-    char *filename = "../tmp/filtered_data.dat";
-
-     // Open the file
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Error while opening file");
-        exit(2);
+    char fileTitle[200]="../tests/";
+    strcat(fileTitle, station);
+    strcat(fileTitle, "_");
+    strcat(fileTitle, consumer);
+    if(strcmp("all", powerplant)!=0){ // if we have a specified power plant
+        strcat(fileTitle, "_");
+        strcat(fileTitle, powerplant);
     }
-    tree = readDataAndBuildAVL(tree, file, isLv, isHva, isHvb);
+    strcat(fileTitle, ".csv");
+    file = fopen(fileTitle, "w+");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        exit(4);
+    }
+    if (isLv) {
+        fprintf(file, "LV Station:Capacity:Consumption (%s)\n", consumer);
+    }
+    if (isHva) {
+        fprintf(file, "HVA Station:Capacity:Consumption (%s)\n", consumer);
+    }
+    if (isHvb) {
+        fprintf(file, "HVB Station:Capacity:Consumption (%s)\n", consumer);
+    }
+    fprintAVL(tree, file);
     fclose(file);
-    return tree;
 }
